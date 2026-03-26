15bc2fac59b1dba5b73ad3ddb729613595eb61b9
Warren Haslam <warren.haslam@gmail.com>
2026-03-26 18:45:57 +0000

    Test worker accepts a POST request with JSON body

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,41 @@
  */
 export default {
 
-	async fetch(request): Promise<Response> {
+	async fetch(request: Request): Promise<Response> {
         const url = new URL(request.url)
+        const method = request.method
+        const contentType = request.headers.get('content-type') || ''
+        console.info(`contentType: ${contentType}`)
+
+        const parseBody = async () => {
+            if (contentType.includes('application/json')) {
+                return await request.json()
+            }
+            else if (contentType.includes('form')) {
+                const formData = await request.formData()
+                const body = {}
+                for (const entry of formData.entries()) {
+                    body[entry[0]] = entry[1]
+                }
+                return body
+            }
+            else {
+                return null
+            }
+        }
 
         const value = url.searchParams.get('test') || null
+        const body = await parseBody()
+        const thingy = JSON.stringify(body)
+        console.info(`body: ${JSON.stringify(body)}`)
 
         const data = {
-            hello: "world",
+            hello: 'world',
+            method,
+            contentType,
             value,
+            body,
+            thingy,
             version: 1.0
         }
         return Response.json(data)
